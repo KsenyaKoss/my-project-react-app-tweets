97c723517ad7021f22f6baa5d99342691265177e
KsenyaKoss <roxenn34@gmail.com>
2023-05-17 22:18:20 +0200

    FIXED: path to  logo

--- src/pages/TweetsPage/StyledTweetsPage.js
+++ src/pages/TweetsPage/StyledTweetsPage.js
@@ -26,8 +26,7 @@ export const StyledButton = styled.button`
 export const StyledWrapper = styled.div`
 display: flex;
 align-items: center;
-justify-content: space-between;
- 
+justify-content: space-between; 
 `
 
 export const StyledRingLoader = styled(RingLoader)`
