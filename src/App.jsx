0e418800d1ceeca28f642885d61e05e3df5a84cb
KsenyaKoss <roxenn34@gmail.com>
2023-05-21 20:27:10 +0200

    DELETED: app.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import {  Route, Routes } from "react-router-dom";
-import "./App.css";
 import HomePage from "./pages/HomePage/HomePage";
 import TweetsPage from "./pages/TweetsPage/TweetsPage";
 function App() {
